ca6e78a4b1514cbe1ce75f36cddcdca14d2152cd
Timothy Johnson <timothy.johnson@broadcom.com>
2022-11-30 14:54:00 -0500

    Refactor success/fail in github plugin
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/github/src/fail.ts
+++ packages/github/src/fail.ts
@@ -19,30 +19,33 @@ import { DEFAULT_RELEASE_LABELS, IPluginConfig } from "./config";
 import { getOctokit } from "./utils";
 
 export default async function (context: IContext, config: IPluginConfig): Promise<void> {
-    if (config.checkPrLabels) {
-        const octokit = getOctokit(context, config);
-        const prs = await octokit.repos.listPullRequestsAssociatedWithCommit({
-            ...context.ci.repo,
-            commit_sha: context.ci.commit
-        });
+    if (!config.checkPrLabels) {
+        return;
+    }
 
-        if (prs.data.length > 0) {
-            const prNumber = prs.data[0].number;
-            const labels = await octokit.issues.listLabelsOnIssue({
+    const octokit = getOctokit(context, config);
+    const prs = await octokit.repos.listPullRequestsAssociatedWithCommit({
+        ...context.ci.repo,
+        commit_sha: context.ci.commit
+    });
+    if (prs.data.length === 0) {
+        return;
+    }
+
+    const prNumber = prs.data[0].number;
+    const labels = await octokit.issues.listLabelsOnIssue({
+        ...context.ci.repo,
+        issue_number: prNumber
+    });
+    const releaseLabels = Array.isArray(config.checkPrLabels) ? config.checkPrLabels : DEFAULT_RELEASE_LABELS;
+
+    for (const { name } of labels.data.filter(label => releaseLabels.includes(label.name))) {
+        await utils.dryRunTask(context, `remove pull request label "${name}"`, async () => {
+            await octokit.issues.removeLabel({
                 ...context.ci.repo,
-                issue_number: prNumber
+                issue_number: prNumber,
+                name
             });
-            const releaseLabels = Array.isArray(config.checkPrLabels) ? config.checkPrLabels : DEFAULT_RELEASE_LABELS;
-
-            for (const { name } of labels.data.filter(label => releaseLabels.includes(label.name))) {
-                await utils.dryRunTask(context, `remove pull request label "${name}"`, async () => {
-                    await octokit.issues.removeLabel({
-                        ...context.ci.repo,
-                        issue_number: prNumber,
-                        name
-                    });
-                });
-            }
-        }
+        });
     }
 }
--- packages/github/src/success.ts
+++ packages/github/src/success.ts
@@ -19,7 +19,7 @@ import { IPluginConfig } from "./config";
 import * as utils from "./utils";
 
 export default async function (context: IContext, config: IPluginConfig): Promise<void> {
-    if (Object.keys(context.releasedPackages).length == 0) {
+    if (Object.keys(context.releasedPackages).length === 0) {
         return;
     }
 
@@ -28,32 +28,33 @@ export default async function (context: IContext, config: IPluginConfig): Promis
         ...context.ci.repo,
         commit_sha: context.ci.commit
     });
+    if (prs.data.length === 0) {
+        return;
+    }
 
-    if (prs.data.length > 0) {
-        await coreUtils.dryRunTask(context, "add released label to pull request", async () => {
-            await octokit.issues.addLabels({
-                ...context.ci.repo,
-                issue_number: prs.data[0].number,
-                labels: ["released"]
-            });
+    await coreUtils.dryRunTask(context, "add released label to pull request", async () => {
+        await octokit.issues.addLabels({
+            ...context.ci.repo,
+            issue_number: prs.data[0].number,
+            labels: ["released"]
         });
+    });
 
-        const packageList: string[] = [];
-        for (const pkgType of Object.keys(context.releasedPackages)) {
-            for (const { name, url } of context.releasedPackages[pkgType]) {
-                const pkgName = url != null ? `[${name}](${url})` : `\`${name}\``;
-                packageList.push(`**${pkgType}**: ${pkgName}`);
-            }
+    const packageList: string[] = [];
+    for (const pkgType of Object.keys(context.releasedPackages)) {
+        for (const { name, url } of context.releasedPackages[pkgType]) {
+            const pkgName = url != null ? `[${name}](${url})` : `\`${name}\``;
+            packageList.push(`**${pkgType}**: ${pkgName}`);
         }
-        await coreUtils.dryRunTask(context, "create success comment on pull request", async () => {
-            await octokit.issues.createComment({
-                ...context.ci.repo,
-                issue_number: prs.data[0].number,
-                body: `Release succeeded for the \`${context.branch.name}\` branch. :tada:\n\n` +
+    }
+    await coreUtils.dryRunTask(context, "create success comment on pull request", async () => {
+        await octokit.issues.createComment({
+            ...context.ci.repo,
+            issue_number: prs.data[0].number,
+            body: `Release succeeded for the \`${context.branch.name}\` branch. :tada:\n\n` +
                 `The following packages have been published:\n` +
                 packageList.map(line => `* ${line}`).join("\n") + `\n\n` +
                 `<sub>Powered by Octorelease :rocket:</sub>`
-            });
         });
-    }
+    });
 }
